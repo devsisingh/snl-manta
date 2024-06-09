d6be91eae3b53faaca47f24dcc4a928b0bc6d2f8
Devsi <grace.emily876@gmail.com>
2024-06-09 23:01:45 +0530

    chain changes

--- src/components/mantaEth.ts
+++ src/components/mantaEth.ts
@@ -1,7 +1,7 @@
 import { Chain } from "@wagmi/core";
 
 export const MantaPacific = {
-  id: 169,
+  id: 3441006,
   name: "Manta Pacific",
   network: "Manta Pacific Mainnet",
   nativeCurrency: {
@@ -11,20 +11,20 @@ export const MantaPacific = {
   },
   rpcUrls: {
     public: {
-      http: ["https://pacific-rpc.manta.network/http"],
+      http: ["https://pacific-rpc.sepolia-testnet.manta.network/http"],
     },
     default: {
-      http: ["https://pacific-rpc.manta.network/http"],
+      http: ["https://pacific-rpc.sepolia-testnet.manta.network/http"],
     },
   },
   blockExplorers: {
     etherscan: {
       name: "Manta Pacific",
-      url: "https://pacific-explorer.manta.network/",
+      url: "https://pacific-explorer.sepolia-testnet.manta.network",
     },
     default: {
       name: "Manta Pacific",
-      url: "https://pacific-explorer.manta.network/",
+      url: "https://pacific-explorer.sepolia-testnet.manta.network",
     },
   },
 } as const satisfies Chain;
